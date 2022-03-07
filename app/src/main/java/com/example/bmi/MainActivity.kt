1d6d72ef658246303f2740f12e49e401b880eae5
Doreen <doreen7932@gmail.com>
2022-03-07 17:00:39 +0800

    GraduatedStudent.kt inherit concept Student.kt simplify expression and when usage MainActivity.kt toast and alert dialog usage

--- app/src/main/java/com/example/bmi/MainActivity.kt
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -2,20 +2,42 @@ package com.example.bmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import com.example.bmi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    var secret = (1..10).random()
+    var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) { //override覆寫 跟父類別方法一樣才能執行
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+//        setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "secret: $secret")
     }
 
     fun guess(view: View) {
-        println("hahaha")
-        println(binding.number.text.toString())
+        counter++
+        binding.tvCounter.text = counter.toString()
+        val num = binding.number.text.toString().toInt()
+        val message = if (num < secret) "Bigger"
+        else if (num > secret) "Smaller"
+        else {
+            secret = (1..10).random()
+            Log.d("MainActivity", "secret: $secret")
+            counter = 0
+            "You got it"
+        }
+        AlertDialog.Builder(this)
+            .setTitle("Guess")
+            .setMessage(message)
+            .setPositiveButton("OK") {dialog, which ->
+                secret = (1..10).random()
+                Log.d("MainActivity", "guess: secret: $secret")
+            }
+            .show()
     }
 }
--- lib/src/main/java/com/example/lib/GraduatedStudent.kt
+++ lib/src/main/java/com/example/lib/GraduatedStudent.kt
@@ -2,9 +2,11 @@ package com.example.lib
 
 class GraduatedStudent constructor(name: String, english: Int, math: Int)
     : Student(name, english, math) {
+    constructor(name: String) : this(name, 0,0)
+    constructor() : this("",0,0)
 }
 
 fun main() {
-    var jenny = GraduatedStudent("Jenny", 70, 90)
+    var jenny = GraduatedStudent("Jenny")
     jenny.print()
 }
