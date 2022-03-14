c11261d1bb795829a696450b5a727720df3c3ed8
Doreen <doreen7932@gmail.com>
2022-03-14 17:03:43 +0800

    MVVM practice

---
+++ app/src/main/java/com/example/bmi/GuessViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.bmi
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GuessViewModel: ViewModel() {
+    private var secret = 0
+    val counter = MutableLiveData<Int>()
+    enum class GameState {
+        INIT, BIGGER, SMALLER, BINGO, END
+    }
+    val gameState = MutableLiveData<GameState>()
+    init {
+        gameState.value = GameState.INIT
+        secret = (1..10).random()
+        counter.value = 0
+        println("secret: $secret")
+    }
+    fun guess(num: Int) {
+        counter.value = counter.value?.plus(1)
+        gameState.value = if (num > secret) GameState.SMALLER
+        else if (num < secret) GameState.BIGGER
+        else GameState.BINGO
+    }
+    fun reset() {
+        secret = (1..10).random()
+        counter.value = 0
+        println("secret: $secret")
+    }
+}
--- app/src/main/java/com/example/bmi/MainActivity.kt
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import com.example.bmi.databinding.ActivityMainBinding
 import kotlin.math.log
@@ -13,37 +14,38 @@ class MainActivity : AppCompatActivity() {
         private val TAG = MainActivity::class.java.simpleName
     }
     private lateinit var binding: ActivityMainBinding
-    var game = NumberGame()
+    val viewModel by viewModels<GuessViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) { //override覆寫 跟父類別方法一樣才能執行
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        viewModel.counter.observe(this) {
+            binding.tvCounter.setText(it.toString())
+        }
+        viewModel.gameState.observe(this) {state ->
+            val message = when(state) {
+                GuessViewModel.GameState.BIGGER -> getString(R.string.bigger)
+                GuessViewModel.GameState.SMALLER -> getString(R.string.smaller)
+                GuessViewModel.GameState.BINGO -> getString(R.string.bingo)
+                GuessViewModel.GameState.INIT -> "Start!"
+                else -> getString(R.string.something_go_wrong)
+            }
+
+            AlertDialog.Builder(this)
+                .setTitle(getString(R.string.dialog_title))
+                .setMessage(message)
+                .setPositiveButton(getString(R.string.ok)) { dialog, which ->
+                    if (state == GuessViewModel.GameState.BINGO)
+                        viewModel.reset()
+                }
+                .show()
+        }
     }
 
     fun guess(view: View) {
-        Log.d(TAG, "Testing: secret is ${game.secret}")
         val num = binding.number.text.toString().toInt()
-        val state = game.guess(num)
-
-        val message = when(state) {
-            NumberGame.GameState.BIGGER -> getString(R.string.bigger)
-            NumberGame.GameState.SMALLER -> getString(R.string.smaller)
-            NumberGame.GameState.BINGO -> getString(R.string.bingo)
-            else -> getString(R.string.something_go_wrong)
-        }
-
-        AlertDialog.Builder(this)
-            .setTitle(getString(R.string.dialog_title))
-            .setMessage(message)
-            .setPositiveButton(getString(R.string.ok)) { dialog, which ->
-                if (game.end) game.reset()
-                updateUI()
-            }
-            .show()
-        updateUI()
-    }
-
-    private fun updateUI() {
-        binding.tvCounter.text = getString(R.string.counter_times, game.counter)
+        viewModel.guess(num)
+//        binding.number.text.clear()
     }
 }
